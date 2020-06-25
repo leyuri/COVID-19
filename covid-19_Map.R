c9561eabcc71da95b672aa3d9578d259be0e82b5
leyuri <leyuri97@gmail.com>
2020-06-25 23:36:15 +0900

    Add Map code

---
+++ covid-19_Map.R
@@ -0,0 +1,27 @@
+route <- read.csv(file = "/Users/yuri/data/route.csv", header=T)
+str(route)
+head(route)
+
+install.packages('devtools')
+library('devtools')
+install_github('dkahle/ggmap')
+library('ggmap')
+
+# google API KEY
+register_google(key='AIzaSyDg4nbtILtXctRHeHWz1YWhz4Bd8ADG8fk')
+
+library('rvest')
+ggmap(get_map(location='south korea', zoom=7))
+
+library('rvest')
+# 기본 map 완성
+map <- ggmap(get_map(location='south korea', zoom=7, color='bw'))
+
+# 지역별 환자수
+map + geom_point(data=route, aes(x=longitude, y=latitude, color=province))
+
+# 지역별 환자의  2차원 밀도를 보여줌
+map + stat_density_2d(data=route, aes(x=longitude, y=latitude))
+
+# 레벨이 높으면 불투명하게(색이 더 잘 드러나게) 칠하고 낮을 때는 투명하게(희미하게)
+map + stat_density_2d(data=route, aes(x=longitude, y=latitude, fill=..level.., alpha=..level..), geom='polygon', size=2, bins=30)
